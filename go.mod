4418e3681b558c1b27a21e515cc2358e7c60c956
ssaxena2609 <ssaxena2609@gmail.com>
2025-05-20 05:52:25 +0530

    pupply calls dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ssaxena2609/puppy
 
 go 1.24.3
+
+require github.com/ssaxena2609/dog v0.0.0-20250519061238-c3e520aa21d4
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import "github.com/ssaxena2609/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!"
+}
+
+func BigBark(s string) string {
+	return dog.WhenGrownUp(Bark())
+}
+func BigBarks(s string) string {
+	return dog.WhenGrownUp(Barks())
 }
